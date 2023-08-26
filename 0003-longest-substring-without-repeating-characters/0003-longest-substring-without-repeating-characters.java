240189676d65f09e58022e0c498438751e774c1b
Preethi Viswanathan <70121351+geekpreet4u@users.noreply.github.com>
2023-08-25 23:18:30 -0400

    Time: 7 ms (58.14%), Space: 43.3 MB (89.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-         int startPointer =0;
-         int endPointer =0;
-         int maxLen = Math.min(s.length(),1);
-   HashSet<Character> aa = new HashSet();
-         while( endPointer < s.length()){
-             char c =s.charAt(endPointer);
-             while(aa.contains(c)){
-                 aa.remove(s.charAt(startPointer));
-                 startPointer++ ;
-             }
-             aa.add(c);
-             int windowSize = endPointer - startPointer + 1;
-             maxLen = Math.max( maxLen, windowSize);
-             endPointer++;
-         }
-      
+       int startPointer =0;
+       int endPointer =0;
+       int maxLen = Math.min(s.length(), 1);
 
-        //abcab sliding window o(n)
+       HashSet<Character> aa = new HashSet ();
+       while(endPointer < s.length() ){
+            char c = s.charAt(endPointer);
+            while (aa.contains(c)){
+                aa.remove(s.charAt(startPointer));
+                startPointer++;
+            }
+            aa.add(c);
+            int windowSize = endPointer - startPointer +1;
+            maxLen = Math.max(  maxLen, windowSize);
+            endPointer++;
 
-        return maxLen;
-        
-    }
+
+       }
+       return maxLen;
+}
 }
